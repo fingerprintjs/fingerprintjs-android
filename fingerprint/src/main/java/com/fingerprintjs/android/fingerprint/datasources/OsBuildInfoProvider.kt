9c8064298b433ebecfa75781815decb10b97792e
Alexey-Verkhovsky <alexey.verkhovsky@mail.ru>
2020-12-30 15:00:02 +0300

    Add datasource with camera info and update osBuildInfoProvided

--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/datasources/OsBuildInfoProvider.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/datasources/OsBuildInfoProvider.kt
@@ -8,19 +8,34 @@ import com.fingerprintjs.android.fingerprint.tools.executeSafe
 interface OsBuildInfoProvider {
     fun modelName(): String
     fun manufacturerName(): String
+    fun androidVersion(): String
+    fun sdkVersion(): String
+    fun kernelVersion(): String
     fun fingerprint(): String
 }
 
 class OsBuildInfoProviderImpl : OsBuildInfoProvider {
     override fun modelName(): String {
-        return executeSafe( { Build.MODEL }, "")
+        return executeSafe({ Build.MODEL }, "")
     }
 
     override fun manufacturerName(): String {
-        return executeSafe( { Build.MANUFACTURER }, "")
+        return executeSafe({ Build.MANUFACTURER }, "")
+    }
+
+    override fun androidVersion(): String {
+        return executeSafe({ Build.VERSION.RELEASE }, "")
+    }
+
+    override fun sdkVersion(): String {
+        return executeSafe({ Build.VERSION.SDK_INT.toString() }, "")
+    }
+
+    override fun kernelVersion(): String {
+        return executeSafe({ System.getProperty("os.version") ?: "" }, "")
     }
 
     override fun fingerprint(): String {
-        return executeSafe( { Build.FINGERPRINT }, "")
+        return executeSafe({ Build.FINGERPRINT }, "")
     }
 }
