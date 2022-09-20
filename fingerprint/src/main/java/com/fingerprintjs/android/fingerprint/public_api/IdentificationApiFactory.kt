bfa49bbcfe7cefdee1cc2b3eb8431a96a0472912
Sergey Makarov <sergeytop100@gmail.com>
2022-09-20 16:52:43 +0300

    Factory update

--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/IdentificationApiFactory.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/IdentificationApiFactory.kt
@@ -1,11 +1,9 @@
 package com.fingerprintjs.android.fingerprint.public_api
 
 import android.content.Context
-import com.fingerprintjs.android.fingerprint.Configuration
 
 public abstract class IdentificationApiFactory internal constructor() {
     public abstract fun create(
         context: Context,
-        configuration: Configuration,
     ): IdentificationApi
 }
