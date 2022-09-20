e05cc0f70f8ab75472083cea9e02e6c276c3538c
Sergey Makarov <sergeytop100@gmail.com>
2022-09-20 16:45:46 +0300

    Async methods

--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/IdentificationApi.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/IdentificationApi.kt
@@ -11,22 +11,29 @@ public abstract class IdentificationApi internal constructor() {
     // interop with java?
     // maybe also provide async api? but we won't want to do so inside interface
     // but maybe using class in enough for this thing
-    public fun getFingerprint(
+    public abstract fun getFingerprint(
         @IdentificationVersionRange(from = IdentificationVersion.V_5) version: IdentificationVersion,
         stabilityLevel: StabilityLevel = StabilityLevel.OPTIMAL,
         hasher: Hasher = MurMur3x64x128Hasher(),
-    ): String {
-        return getFingerprint(
-            signals = getFingerprintingSignalsProvider().getSignalsMatching(version, stabilityLevel)
-        )
-    }
+    )
 
-    public fun getFingerprint(
+    public abstract fun getFingerprint(
         signals: List<FingerprintingSignal>,
         hasher: Hasher = MurMur3x64x128Hasher(),
-    ): String {
-        return TODO()
-    }
+    )
+
+    public abstract fun getFingerprintAsync(
+        @IdentificationVersionRange(from = IdentificationVersion.V_5) version: IdentificationVersion,
+        stabilityLevel: StabilityLevel = StabilityLevel.OPTIMAL,
+        hasher: Hasher = MurMur3x64x128Hasher(),
+        listener: (String) -> Unit,
+    )
+
+    public abstract fun getFingerprintAsync(
+        signals: List<FingerprintingSignal>,
+        hasher: Hasher = MurMur3x64x128Hasher(),
+        listener: (String) -> Unit,
+    )
 
     // same concerns
     public fun getDeviceId(
@@ -34,6 +41,11 @@ public abstract class IdentificationApi internal constructor() {
     ): String {
         return getDeviceIdProvider().getDeviceIdMatching(version)
     }
+
+    public abstract fun getDeviceIdAsync(
+        @IdentificationVersionRange(from = IdentificationVersion.V_1) version: IdentificationVersion,
+        listener: (String) -> Unit,
+    )
 
     public abstract fun getDeviceIdProvider(): DeviceIdProvider
     public abstract fun getFingerprintingSignalsProvider(): FingerprintingSignalsProvider
