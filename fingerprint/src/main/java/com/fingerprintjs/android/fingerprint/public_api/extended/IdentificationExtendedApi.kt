cecd6a0d7a16846dc7148d759e8b3302ae9c02a5
Sergey Makarov <sergeytop100@gmail.com>
2022-09-20 15:38:45 +0300

    Extended api interface -> class

--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/extended/IdentificationExtendedApi.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/extended/IdentificationExtendedApi.kt
@@ -6,20 +6,20 @@ import com.fingerprintjs.android.fingerprint.signal_providers.StabilityLevel
 import com.fingerprintjs.android.fingerprint.tools.hashers.Hasher
 import com.fingerprintjs.android.fingerprint.tools.hashers.MurMur3x64x128Hasher
 
-public interface IdentificationExtendedApi {
-    public fun getDeviceIdProvider(): DeviceIdProvider
-    public fun getSignalsProvider(): SignalsProvider
+public abstract class IdentificationExtendedApi internal constructor() {
+    public abstract fun getDeviceIdProvider(): DeviceIdProvider
+    public abstract fun getSignalsProvider(): SignalsProvider
 
-    public fun DeviceIdProvider.getDeviceIdFor(
+    public abstract fun DeviceIdProvider.getDeviceIdFor(
         version: Int,
     ): String
 
-    public fun SignalsProvider.getSignalsFor(
+    public abstract fun SignalsProvider.getSignalsFor(
         version: IdentificationVersion,
         stabilityLevel: StabilityLevel,
     ): List<IdentificationSignalV2>
 
-    public fun List<IdentificationSignalV2>.getFingerprint(
+    public abstract fun List<IdentificationSignalV2>.getFingerprint(
         hasher: Hasher = MurMur3x64x128Hasher()
     ): String
 }
