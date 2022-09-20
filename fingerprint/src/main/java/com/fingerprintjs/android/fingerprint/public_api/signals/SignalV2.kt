75a687213996c503f8f312ce8742023f577283ee
Sergey Makarov <sergeytop100@gmail.com>
2022-09-20 17:00:42 +0300

    Add value property to signal

--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/signals/SignalV2.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/public_api/signals/SignalV2.kt
@@ -5,8 +5,9 @@ import com.fingerprintjs.android.fingerprint.signal_providers.StabilityLevel
 // think about keeping generic value in the signal
 // pros: sometimes even handling raw value of Any type may make sense. for example, to call JSONObject.wrap(value)
 // cons: we will always need to wrap signal's data into some class, which might get cumbersome
-public sealed class FingerprintingSignal() {
+public sealed class FingerprintingSignal<T>() {
     public abstract val fingerprintingInfo: FingerprintingInfo
+    public abstract val value: T
     public abstract fun getHashableString(): String
 }
 
