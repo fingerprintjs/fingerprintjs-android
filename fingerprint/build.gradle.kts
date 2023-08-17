e884fd956b728c86426f68f05fae3724b04ce6a4
Sergey Makarov <sergeytop100@gmail.com>
2023-08-17 12:13:53 +0300

    Disable proguard for the library module. Was enabled by accident when moving to Kotlin DSL

--- fingerprint/build.gradle.kts
+++ fingerprint/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
