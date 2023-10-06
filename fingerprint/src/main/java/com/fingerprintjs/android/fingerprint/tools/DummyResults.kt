900efb0936df4fe28b3fb9a141a4c5cda3c2b257
Sergey Makarov <sergeytop100@gmail.com>
2023-10-06 19:14:41 +0300

    Dummy results: replace zeros with empty string

--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/tools/DummyResults.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/tools/DummyResults.kt
@@ -5,8 +5,8 @@ import com.fingerprintjs.android.fingerprint.FingerprintResult
 import com.fingerprintjs.android.fingerprint.signal_providers.SignalGroupProvider
 
 internal object DummyResults {
-    private const val ALL_ZEROS = "0000000000000000"
-    const val fingerprint = ALL_ZEROS
+    private const val dummyString = ""
+    const val fingerprint = dummyString
     val fingerprintResult = object : FingerprintResult {
         override val fingerprint: String
             get() = this@DummyResults.fingerprint
@@ -16,9 +16,9 @@ internal object DummyResults {
         }
     }
     val deviceIdResult = DeviceIdResult(
-        deviceId = ALL_ZEROS,
-        gsfId = ALL_ZEROS,
-        androidId = ALL_ZEROS,
-        mediaDrmId = ALL_ZEROS,
+        deviceId = dummyString,
+        gsfId = dummyString,
+        androidId = dummyString,
+        mediaDrmId = dummyString,
     )
 }
